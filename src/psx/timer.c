96f410acc364eaf3314238c65403a5eddcab5874
Pichuman10 <107434295+Pichuman100@users.noreply.github.com>
2022-11-18 09:56:11 -0500

    Update timer.c

--- src/psx/timer.c
+++ src/psx/timer.c
@@ -113,24 +113,14 @@ void StageTimer_Calculate()
 
 void StageTimer_Tick()
 {
-	if (!stage.paused)
-	{
-		timer.secondtimer += timer_dt / 12;
-		if (stage.prefs.palmode ? timer.secondtimer >= 50 : timer.secondtimer >= 60)
-		{
-			timer.secondtimer = 0;
-			if (timer.timer <= 0)
-			{		
-				if (timer.timermin > 0)
-					timer.timermin --;
-				else
-					timer.timermin = 0;
-				timer.timer = 59;
-			}
-			else 
-				timer.timer --;
-		}
-	}
+	//im deeply sorry for anyone reading this code
+	//has the song started?
+	if (stage.song_step > 0) //if song starts decrease the timer
+		timer.timer = (Audio_GetLength(stage.stage_def->music_track)+1) - (stage.song_time / 1000); //seconds (ticks down)
+	else //if not keep the timer at the song starting length    
+		timer.timer = (Audio_GetLength(stage.stage_def->music_track)+1); //seconds (ticks down)
+	timer.timermin = timer.timer / 60; //minutes left till song ends
+	timer.timersec = timer.timer % 60; //seconds left till song ends
 }
 
 void StageTimer_Draw()
@@ -148,14 +138,15 @@ void StageTimer_Draw()
 	sprintf(timer.timer_display, ":");
 	stage.font_cdr.draw(&stage.font_cdr,
 		timer.timer_display,
+
 		FIXED_DEC(-1,1) + stage.noteshakex,
 		FIXED_DEC(-109,1) + stage.noteshakey,
 		FontAlign_Left
 	);
-	if (timer.timer >= 10)
-		sprintf(timer.timer_display, "%d", timer.timer);
+	if (timer.timersec >= 10)
+		sprintf(timer.timer_display, "%d", timer.timersec);
 	else
-		sprintf(timer.timer_display, "0%d", timer.timer);
+		sprintf(timer.timer_display, "0%d", timer.timersec);
 
 	stage.font_cdr.draw(&stage.font_cdr,
 		timer.timer_display,
