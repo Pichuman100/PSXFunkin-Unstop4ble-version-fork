9209b2f97639641e78e89409bcac99ac95c48758
Pichuman10 <107434295+Pichuman100@users.noreply.github.com>
2023-03-24 08:15:56 -0400

    Update stagedef_disc1.h

--- src/stagedef_disc1.h
+++ src/stagedef_disc1.h
@@ -1,4 +1,4 @@
-	{ //StageId_1_1 (Bopeebo)
+	{ //StageId_Bopeebo (Bopeebo)
 		//Characters
 		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
         {NULL},
@@ -13,9 +13,9 @@
 		1, 1,
 		XA_Bopeebo, 0,
 		
-		StageId_1_2, STAGE_LOAD_FLAG
+		StageId_Fresh, STAGE_LOAD_FLAG
 	},
-	{ //StageId_1_2 (Fresh)
+	{ //StageId_Fresh (Fresh)
 		//Characters
 		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
         {NULL},
@@ -30,9 +30,9 @@
 		1, 2,
 		XA_Fresh, 2,
 		
-		StageId_1_3, STAGE_LOAD_FLAG
+		StageId_Dadbattle, STAGE_LOAD_FLAG
 	},
-	{ //StageId_1_3 (Dadbattle)
+	{ //StageId_Dadbattle (Dadbattle)
 		//Characters
 		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
         {NULL},
@@ -47,9 +47,9 @@
 		1, 3,
 		XA_Dadbattle, 0,
 		
-		StageId_1_3, 0
+		StageId_Dadbattle, 0
 	},
-	{ //StageId_1_4 (Tutorial)
+	{ //StageId_Tutorial (Tutorial)
 		//Characters
 		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
         {NULL},
@@ -64,282 +64,5 @@
 		1, 4,
 		XA_Tutorial, 2,
 		
-		StageId_1_4, 0
-	},
-	
-	{ //StageId_2_1 (Spookeez)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		2, 1,
-		XA_Spookeez, 0,
-		
-		StageId_2_2, STAGE_LOAD_FLAG
-	},
-	{ //StageId_2_2 (South)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		2, 2,
-		XA_South, 2,
-		
-		StageId_2_3, STAGE_LOAD_FLAG | STAGE_LOAD_OPPONENT
-	},
-	{ //StageId_2_3 (Monster)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		2, 3,
-		XA_Monster, 0,
-		
-		StageId_2_3, 0
-	},
-	
-	{ //StageId_3_1 (Pico)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		3, 1,
-		XA_Pico, 0,
-		
-		StageId_3_2, STAGE_LOAD_FLAG
-	},
-	{ //StageId_3_2 (Philly)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		3, 2,
-		XA_Philly, 2,
-		
-		StageId_3_3, STAGE_LOAD_FLAG
-	},
-	{ //StageId_3_3 (Blammed)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		3, 3,
-		XA_Blammed, 0,
-		
-		StageId_3_3, 0
-	},
-	
-	{ //StageId_4_1 (Satin Panties)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		4, 1,
-		XA_SatinPanties, 0,
-		
-		StageId_4_2, STAGE_LOAD_FLAG
-	},
-	{ //StageId_4_2 (High)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		4, 2,
-		XA_High, 2,
-		
-		StageId_4_3, STAGE_LOAD_FLAG
-	},
-	{ //StageId_4_3 (MILF)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		4, 3,
-		XA_MILF, 0,
-		
-		StageId_4_3, 0
-	},
-	{ //StageId_4_4 (Test)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		4, 4,
-		XA_Test, 2,
-		
-		StageId_4_4, 0
-	},
-	
-	{ //StageId_5_1 (Cocoa)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		5, 1,
-		XA_Cocoa, 0,
-		
-		StageId_5_2, STAGE_LOAD_FLAG
-	},
-	{ //StageId_5_2 (Eggnog)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		5, 2,
-		XA_Eggnog, 2,
-		
-		StageId_5_3, STAGE_LOAD_FLAG | STAGE_LOAD_OPPONENT | STAGE_LOAD_STAGE
-	},
-	{ //StageId_5_3 (Winter Horrorland)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		5, 3,
-		XA_WinterHorrorland, 0,
-		
-		StageId_5_3, 0
-	},
-	
-	{ //StageId_6_1 (Senpai)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		6, 1,
-		XA_Senpai, 0,
-		
-		StageId_6_2, STAGE_LOAD_FLAG | STAGE_LOAD_OPPONENT
-	},
-	{ //StageId_6_2 (Roses)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		6, 2,
-		XA_Roses, 2,
-		
-		StageId_6_3, 0
-	},
-	{ //StageId_6_3 (Thorns)
-		//Characters
-		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
-        {NULL},
-		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
-		
-		//Stage background
-		Back_Week1_New,
-		
-		//Song info
-		6, 3,
-		XA_Thorns, 0,
-		
-		StageId_6_3, 0
+		StageId_Tutorial, 0
 	},
